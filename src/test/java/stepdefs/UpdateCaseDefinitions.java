d5cb68b25941744f6fd3f8c5d801f6a515434fae
pega-elvad <david.elvarcalamonte@pega.com>
2019-11-07 00:54:51 +0100

    first version

---
+++ src/test/java/stepdefs/UpdateCaseDefinitions.java
@@ -0,0 +1,113 @@
+package stepdefs;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UpdateCaseDefinitions {
+
+    private RequestSpecification request;
+    private Response response;
+
+    private String nextAssignmentID;
+    private String ENDPOINT = "https://lab0625.lab.pega.com:443/prweb/api/v1";
+
+    @Given("^A new update customer case type$")
+    public void a_new_update_customer_case_type() throws Exception {
+        JSONObject jsonObj = new JSONObject()
+                .put("caseTypeID", "MyOrg-DemoApp-Work-UpdateCustomer")
+                .put("processID", "pyStartCase");
+
+        nextAssignmentID =
+                given()
+                        .baseUri(ENDPOINT)
+                        .contentType(ContentType.JSON)
+                        .auth().basic("demo@pega.com", "ecc2019!")
+                        .body(jsonObj.toString())
+                .when()
+                        .post("/cases")
+                .then()
+                        .statusCode(201)
+                        .extract()
+                        .body()
+                        .path("nextAssignmentID");
+    }
+
+    @When("^I look for a customer with ID \"([^\"]*)\"$")
+    public void i_look_for_a_customer_with_ID(String arg1) throws Exception {
+        JSONObject jsonObj = new JSONObject()
+                .put("content", new JSONObject()
+                        .put("SelectedCustomerID", arg1));
+
+        response =
+                given()
+                        .baseUri(ENDPOINT)
+                        .contentType(ContentType.JSON)
+                        .auth().basic("demo@pega.com", "ecc2019!")
+                        .body(jsonObj.toString())
+                .when()
+                        .post("/assignments/" + nextAssignmentID + "?actionID=SearchCustomer");
+    }
+
+    @When("^I update the email address with \"([^\"]*)\"$")
+    public void i_update_the_email_address_with(String arg1) throws Exception {
+        JSONObject jsonObj = new JSONObject()
+                .put("content", new JSONObject()
+                        .put("CustomerInfo", new JSONObject()
+                                .put("CustomerID", "51425410D")
+                                .put("FirstName", "David")
+                                .put("LastName", "Elvar")
+                                .put("SecondName", "Calamonte")
+                                .put("PhoneNumber", "658 917 059")
+                ));
+
+        response =
+                given()
+                        .baseUri(ENDPOINT)
+                        .contentType(ContentType.JSON)
+                        .auth().basic("demo@pega.com", "ecc2019!")
+                        .body(jsonObj.toString())
+                .when()
+                        .post("/assignments/" + nextAssignmentID + "?actionID=UpdateCustomer");
+    }
+
+    @Then("^The customer profile will be updated$")
+    public void the_customer_profile_will_be_updated() throws Exception {
+        nextAssignmentID =
+                response.then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .path("nextAssignmentID");
+    }
+
+    @Then("^The customer profile will found$")
+    public void the_customer_profile_will_found() throws Exception {
+        nextAssignmentID =
+                response.then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .path("nextAssignmentID");
+    }
+
+    @Then("^The customer profile will not found$")
+    public void the_customer_profile_will_not_found() throws Exception {
+        response
+                .then()
+                    .assertThat()
+                    .statusCode(400)
+                .and()
+                    .contentType(ContentType.JSON).
+                and()
+                    .body("errors[0].ID", equalTo("Pega_API_055"));
+    }
+}
