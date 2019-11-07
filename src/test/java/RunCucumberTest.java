d2b6ed5bfbfdfeb19e8ab646228b9d021f21e377
pega-elvad <david.elvarcalamonte@pega.com>
2019-11-07 23:52:06 +0100

    aa

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources"}, plugin = {"pretty", "html:cucumber-htmlreport"})
+@CucumberOptions(features = {"src/test/resources"}, plugin = {"pretty", "html:target/cucumber-htmlreport", "json:target/cucumber.json"})
 public class RunCucumberTest {
 }
